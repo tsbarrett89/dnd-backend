65349fe77ff71f90a904c7d09130313cf47f3593
tsbarrett89 <tsbarrett89@gmail.com>
2020-07-19 12:45:25 -0700

    characters table dropped on down

--- data/migrations/20200719121837_characters.js
+++ data/migrations/20200719121837_characters.js
@@ -1,8 +1,20 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('characters', tbl => {
+        tbl.increments();
+        tbl.integer('user_id')
+            .unsigned()
+            .references('id')
+            .inTable('users')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE')
+        tbl.string('character_name')
+            .notNullable()
+        tbl.string('level')
+            .notNullable()
+  })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('characters')
 };
