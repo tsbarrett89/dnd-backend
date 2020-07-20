fa0d56a4a36b5e7f200a9ee810f99f3718f3a6b6
tsbarrett89 <tsbarrett89@gmail.com>
2020-07-19 21:24:43 -0700

    races table created and dropped

--- data/migrations/20200719201752_races.js
+++ data/migrations/20200719201752_races.js
@@ -1,8 +1,27 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('races', tbl => {
+        tbl.increments()
+        tbl.string('race')
+            .notNullable()
+            .unique()
+        tbl.text('description')
+            .notNullable()
+        tbl.string('ability_score_increase')
+            .notNullable()
+        tbl.text('age')
+            .notNullable()
+        tbl.text('alignment')
+            .notNullable()
+        tbl.text('size')
+            .notNullable()
+        tbl.integer('speed')
+            .notNullable()
+        tbl.integer('darkvision')
+            .defaultTo(0)
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('races')
 };
