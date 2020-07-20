594f641572d4a64423b4ebab1554041b611c0192
tsbarrett89 <tsbarrett89@gmail.com>
2020-07-19 21:18:39 -0700

    alignment table created and dropped

--- data/migrations/20200719201730_alignment.js
+++ data/migrations/20200719201730_alignment.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('alignment', tbl => {
+        tbl.increments()
+        tbl.string('alignment')
+            .notNullable()
+            .unique()
+        tbl.text('description')
+            .notNullable()
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('alignment')
 };
