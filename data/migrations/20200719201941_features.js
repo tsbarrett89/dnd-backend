efdda0a01e1710b4f7e24ee5bb45ab5dabe466fa
tsbarrett89 <tsbarrett89@gmail.com>
2020-07-19 22:00:52 -0700

    features table created and dropped

--- data/migrations/20200719201941_features.js
+++ data/migrations/20200719201941_features.js
@@ -1,8 +1,23 @@
-
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('features', tbl => {
+        tbl.increments()
+        tbl.integer('class_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('classes')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE')
+        tbl.string('name')
+            .notNullable()
+        tbl.integer('level')
+            .unsigned()
+            .notNullable()
+        tbl.text('description')
+            .notNullable()
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('features')
 };
