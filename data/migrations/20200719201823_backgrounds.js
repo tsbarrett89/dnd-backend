085f77ac50bb17f768faa6293bf7e4dcb50f5b94
tsbarrett89 <tsbarrett89@gmail.com>
2020-07-19 21:36:31 -0700

    backgrounds table created and dropped

--- data/migrations/20200719201823_backgrounds.js
+++ data/migrations/20200719201823_backgrounds.js
@@ -1,8 +1,21 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('backgrounds', tbl => {
+        tbl.increments()
+        tbl.string('background')
+            .notNullable()
+            .unique()
+        tbl.string('skill_proficiencies')
+            .notNullable()
+        tbl.string('tool_proficiencies')
+            .notNullable()
+        tbl.string('languages')
+            .notNullable()
+        tbl.text('equipment')
+            .notNullable()
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('backgrounds')
 };
