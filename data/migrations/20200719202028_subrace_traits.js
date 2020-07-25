3556d4a0f933f7079aa962860fb64776a9b671f7
tsbarrett89 <tsbarrett89@gmail.com>
2020-07-24 17:32:54 -0700

    subrace traits created and dropped

--- data/migrations/20200719202028_subrace_traits.js
+++ data/migrations/20200719202028_subrace_traits.js
@@ -1,6 +1,19 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('subrace_traits', tbl => {
+        tbl.increments()
+        tbl.integer('subrace_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('subraces')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE')
+        tbl.string('trait')
+            .notNullable()
+        tbl.text('description')
+            .notNullable()
+    })
 };
 
 exports.down = function(knex) {
