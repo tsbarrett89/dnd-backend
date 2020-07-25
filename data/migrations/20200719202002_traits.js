5b49d00692beda9ce7162281c64d088ef8fcf945
tsbarrett89 <tsbarrett89@gmail.com>
2020-07-24 17:18:15 -0700

    traits created and dropped

--- data/migrations/20200719202002_traits.js
+++ data/migrations/20200719202002_traits.js
@@ -1,8 +1,21 @@
 
 exports.up = function(knex) {
-  
+    return knex.schem.createTable('traits', tbl => {
+        tbl.increments()
+        tbl.integer('race_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('races')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE')
+        tbl.string('name')
+            .notNullable()
+        tbl.text('description')
+            .notNullable()
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.scehma.dropTableIfExists('traits')
 };
