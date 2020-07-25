4a66e40d93f86819b5eaa34b820fa6c016b2aa2c
tsbarrett89 <tsbarrett89@gmail.com>
2020-07-24 17:23:42 -0700

    subraces created and dropped

--- data/migrations/20200719202011_subraces.js
+++ data/migrations/20200719202011_subraces.js
@@ -1,8 +1,22 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('subraces', tbl => {
+        tbl.increments()
+        tbl.integer('race_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('races')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE')
+        tbl.string('subrace')
+            .notNullable()
+            .unique()
+        tbl.string('ability_score_increase')
+            .defaultTo('none')
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('subraces')
 };
