9df9dbf14e78977804eb0e7ba48a511f39b9b61f
tsbarrett89 <tsbarrett89@gmail.com>
2020-07-26 00:47:46 -0600

    weapon_properties created and dropped

--- data/migrations/20200719202348_weapon_properties.js
+++ data/migrations/20200719202348_weapon_properties.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('weapon_properties', tbl => {
+        tbl.increments()
+        tbl.string('property')
+            .notNullable()
+            .unique()
+        tbl.text('description')
+            .notNullable()
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('weapon_properties')
 };
