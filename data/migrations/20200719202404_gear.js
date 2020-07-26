c42463cc9fd3d4cb553146995805381786b4d07e
tsbarrett89 <tsbarrett89@gmail.com>
2020-07-26 00:51:36 -0600

    gear table created and dropped

--- data/migrations/20200719202404_gear.js
+++ data/migrations/20200719202404_gear.js
@@ -1,8 +1,19 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('gear', tbl => {
+        tbl.increments()
+        tbl.string('name')
+            .notNullable()
+            .unique()
+        tbl.string('cost')
+            .notNullable()
+        tbl.string('weight')
+            .notNullable()
+        tbl.text('description')
+            .notNullable()
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('gear')
 };
