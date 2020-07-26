30f7bf179b5fbe7dfdeb47fa6122e576022f58e3
tsbarrett89 <tsbarrett89@gmail.com>
2020-07-26 00:24:57 -0600

    skills created and dropped

--- data/migrations/20200719202152_skills.js
+++ data/migrations/20200719202152_skills.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('skills', tbl => {
+        tbl.increments()
+        tbl.string('skill')
+            .notNullable()
+            .unique()
+        tbl.string('ability_modifier')
+            .notNullable()
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('skills')
 };
