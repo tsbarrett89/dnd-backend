4e1776784590c4de449d9f90dd19d2a2064113e8
tsbarrett89 <tsbarrett89@gmail.com>
2020-07-26 01:12:10 -0600

    character_classes created and dropped

--- data/migrations/20200719202507_many_to_many.js
+++ data/migrations/20200719202507_many_to_many.js
@@ -1,8 +1,27 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('character_classes', tbl => {
+        tbl.increments()
+        tbl.integer('character_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('characters')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE')
+        tbl.integer('class_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('classes')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE')
+        tbl.integer('class_level')
+            .unsigned()
+            .notNullable()
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('character_classes')
 };
