dc7f3239608332e31e15123f6bc241d26f322cab
tsbarrett89 <tsbarrett89@gmail.com>
2020-07-26 00:36:46 -0600

    armor table created and dropped

--- data/migrations/20200719202259_armor.js
+++ data/migrations/20200719202259_armor.js
@@ -1,8 +1,31 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('armor', tbl => {
+        tbl.increments()
+        tbl.string('armor')
+            .notNullable()
+            .unique()
+        tbl.string('armor_type')
+            .notNullable()
+        tbl.string('cost')
+            .notNullable()
+        tbl.integer('armor_class')
+            .unsigned()
+            .notNullable()
+        tbl.boolean('add_dex')
+            .notNullable()
+        tbl.boolean('full_dex')
+            .notNullable()
+        tbl.integer('strenght_req')
+            .unsigned()
+            .defaultTo(0)
+        tbl.boolean('steal_disadvantage')
+            .defaultTo(false)
+        tbl.string('weight')
+            .notNullable()
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('armor')
 };
