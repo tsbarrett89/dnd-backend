caa961bbffd792122f9a2db331f6c309119832ab
tsbarrett89 <tsbarrett89@gmail.com>
2020-07-26 00:41:50 -0600

    weapons table created and dropped

--- data/migrations/20200719202313_weapons.js
+++ data/migrations/20200719202313_weapons.js
@@ -1,8 +1,23 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('weapons', tbl => {
+        tbl.increments()
+        tbl.string('weapon')
+            .notNullable()
+            .unique()
+        tbl.string('weapon_type')
+            .notNullable()
+        tbl.string('cost')
+            .notNullable()
+        tbl.string('damage')
+            .notNullable()
+        tbl.string('damage_type')
+            .notNullable()
+        tbl.string('weight')
+            .notNullable()
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('weapons')
 };
