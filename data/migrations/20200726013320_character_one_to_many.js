ce226b4af1b0da3b234c8f1c6367001590e758c7
tsbarrett89 <tsbarrett89@gmail.com>
2020-07-27 15:39:22 -0600

    character_abilities created and dropped

--- data/migrations/20200726013320_character_one_to_many.js
+++ data/migrations/20200726013320_character_one_to_many.js
@@ -1,8 +1,22 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('character_abilities', tbl => {
+        tbl.increments()
+        tbl.integer('character_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('characters')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE')
+        tbl.string('ability')
+            .notNullable()
+        tbl.integer('ability_score')
+            .unsigned()
+            .notNullable()
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('character_abilities')
 };
