f6f4061dcfdb422a9e5080584043008d45c8ec4a
tsbarrett89 <tsbarrett89@gmail.com>
2020-07-28 23:08:51 -0600

    changed class_name to class

--- data/migrations/20200719124107_classes.js
+++ data/migrations/20200719124107_classes.js
@@ -1,7 +1,7 @@
 exports.up = function(knex) {
     return knex.schema.createTable('classes', tbl => {
         tbl.increments();
-        tbl.string('class_name')
+        tbl.string('class')
             .notNullable()
             .unique()
         tbl.string('hit_dice')
