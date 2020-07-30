a61b84e63c2d837d6856c89e3483af65c6ac0773
tsbarrett89 <tsbarrett89@gmail.com>
2020-07-30 14:08:12 -0600

    criminal seed

--- data/seeds/01-backgrounds.js
+++ data/seeds/01-backgrounds.js
@@ -9,7 +9,7 @@ exports.seed = function(knex) {
           background: "acolyte",
           skill_proficiencies: "insight, religion",
           tool_proficiencies: "none",
-          languages: "two of your choice",
+          languages: "Two of your choice",
           equipment: "A holy symbol (a gift to you when you entered the priesthood), a prayer book or prayer wheel, 5 sticks of incense, vestments, a set of common clothes, and a pouch containing 15 gp."
         },
         {
@@ -20,7 +20,11 @@ exports.seed = function(knex) {
           equipment: "A set of fine clothes, a disguise kit, tools of the con or your choice (ten stoppered bottles filled with colored liquid, a set of weighted dice, a deck of marked cards, or a signet ring of an imaginary duke), and a pouch containing 15gp."
         },
         {
-          background: ""
+          background: "criminal",
+          skill_proficiencies: "deception, stealth",
+          tool_proficiencies: "One type of gaming set, thieves' tools",
+          languages: "none",
+          equipment: "A crowbar, a set of dark common clothes including a hood, and a pouch containing 15 gp"
         }
       ]);
     });
