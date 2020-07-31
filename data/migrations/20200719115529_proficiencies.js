72ecb7cd235f462ddc1295d3079c9d05a3d4a367
tsbarrett89 <tsbarrett89@gmail.com>
2020-07-31 17:13:14 -0600

    proficiencies table created and dropped

--- data/migrations/20200719115529_proficiencies.js
+++ data/migrations/20200719115529_proficiencies.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('proficiencies', tbl => {
+        tbl.increments()
+        tbl.string('proficiency')
+            .unique()
+            .notNullable()
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('proficiencies')
 };
