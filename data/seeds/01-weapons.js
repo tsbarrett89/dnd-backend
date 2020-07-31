95346d6a334ea2d3172815503b567361c9cd4e2e
tsbarrett89 <tsbarrett89@gmail.com>
2020-07-31 13:48:08 -0600

    club to dagger seeds

--- data/seeds/01-weapons.js
+++ data/seeds/01-weapons.js
@@ -1,13 +1,26 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('weapons').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('weapons').insert([
+        {
+          weapon: "club",
+          weapon_type: "simple melee",
+          cost: "1 sp",
+          damage: "1d4",
+          damage_type: "bludgeoning",
+          weight: "2 lb."
+        },
+        {
+          weapon: "dagger",
+          weapon_type: "simple melee",
+          cost: "2 gp",
+          damage: "1d4",
+          damage_type: "piercing",
+          weight: "1 lb."
+        },
       ]);
     });
 };
