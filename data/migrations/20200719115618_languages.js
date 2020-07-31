52333d5386b834c47c4cadde3fd04dc11db99914
tsbarrett89 <tsbarrett89@gmail.com>
2020-07-31 17:25:37 -0600

    langauges table created and dropped

--- data/migrations/20200719115618_languages.js
+++ data/migrations/20200719115618_languages.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('languages', tbl => {
+        tbl.increment()
+        tbl.string('language')
+            .unique()
+            .notNullable()
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('languages')
 };
