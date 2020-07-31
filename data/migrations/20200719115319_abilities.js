5505779324ee5512a5d3a02fbc0749ed188da5ec
tsbarrett89 <tsbarrett89@gmail.com>
2020-07-31 17:07:50 -0600

    abilities table created and dropped

--- data/migrations/20200719115319_abilities.js
+++ data/migrations/20200719115319_abilities.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('abilities', tbl => {
+        tbl.increments()
+        tbl.string('ability')
+            .unique()
+            .notNullable()
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('abilities')
 };
