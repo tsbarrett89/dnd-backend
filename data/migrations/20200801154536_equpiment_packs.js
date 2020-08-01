908747bdcb2396b8aa89d9a84038e19dce977586
tsbarrett89 <tsbarrett89@gmail.com>
2020-08-01 15:47:21 -0600

    equipment packs table created and dropped

---
+++ data/migrations/20200801154536_equpiment_packs.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('equipment_packs', tbl => {
+        tbl.increments()
+        tbl.string('pack')
+            .unique()
+            .notNullable()
+        tbl.text('contents')
+            .notNullable
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('equipment_packs')
+};
