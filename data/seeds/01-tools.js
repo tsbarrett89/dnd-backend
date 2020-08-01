7a0214e02f03960e6e55fb7de0da16c5e24956ac
tsbarrett89 <tsbarrett89@gmail.com>
2020-08-01 14:07:57 -0600

    finished tools seeds

--- data/seeds/01-tools.js
+++ data/seeds/01-tools.js
@@ -72,6 +72,94 @@ exports.seed = function(knex) {
         {
           tool: "woodcarver's tools",
           type: "artisan's tools"
+        },
+        {
+          tool: "disguise kit",
+          type: "none"
+        },
+        {
+          tool: "forgery kit",
+          type: "none"
+        },
+        {
+          tool: "dice set",
+          type: "gaming set"
+        },
+        {
+          tool: "dragonchess set",
+          type: "gaming set"
+        },
+        {
+          tool: "playing card set",
+          type: "gaming set"
+        },
+        {
+          tool: "three-dragon ante set",
+          type: "gaming set"
+        },
+        {
+          tool: "herbalism kit",
+          type: "none"
+        },
+        {
+          tool: "bagpipes",
+          type: "musical instrument"
+        },
+        {
+          tool: "drum",
+          type: "musical instrument"
+        },
+        {
+          tool: "dulcimer",
+          type: "musical instrument"
+        },
+        {
+          tool: "flute",
+          type: "musical instrument"
+        },
+        {
+          tool: "lute",
+          type: "musical instrument"
+        },
+        {
+          tool: "lyre",
+          type: "musical instrument"
+        },
+        {
+          tool: "horn",
+          type: "musical instrument"
+        },
+        {
+          tool: "pan flute",
+          type: "musical instrument"
+        },
+        {
+          tool: "shawm",
+          type: "musical instrument"
+        },
+        {
+          tool: "viol",
+          type: "musical instrument"
+        },
+        {
+          tool: "navigator's tools",
+          type: "none"
+        },
+        {
+          tool: "poisoner's kit",
+          type: "none"
+        },
+        {
+          tool: "thieve's tools",
+          type: "none"
+        },
+        {
+          tool: "Vehicles (land)",
+          type: "none"
+        },
+        {
+          tool: "Vehicles (water)",
+          type: "none"
         }
       ]);
     });
