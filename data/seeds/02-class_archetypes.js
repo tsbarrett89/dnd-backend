be867e389cf7489f816a38b88bf6964489d0c3a7
tsbarrett89 <tsbarrett89@gmail.com>
2020-08-02 14:27:29 -0600

    bard and cleric archetype seeds added

--- data/seeds/02-class_archetypes.js
+++ data/seeds/02-class_archetypes.js
@@ -1,13 +1,86 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('class_archetypes').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('class_archetypes').insert([
+        {
+          class_id: 1,
+          archetype: "Path of the Berserker"
+        },
+        {
+          class_id: 1,
+          archetype: "Path of the Totem Warrior"
+        },
+        {
+          class_id: 1,
+          archetype: "Path of the Ancestral Guardian"
+        },
+        {
+          class_id: 1,
+          archetype: "Path of the Storm Herald"
+        },
+        {
+          class_id: 1,
+          archetype: "Path of the Zealot"
+        },
+        {
+          class_id: 2,
+          archetype: "College of Lore"
+        },
+        {
+          class_id: 2,
+          archetype: "College of Valor"
+        },
+        {
+          class_id: 2,
+          archetype: "College of Glamour"
+        },
+        {
+          class_id: 2,
+          archetype: "College of Swords"
+        },
+        {
+          class_id: 2,
+          archetype: "College of Whispers"
+        },
+        {
+          class_id: 3,
+          archetype: "Knowledge Domain"
+        },
+        {
+          class_id: 3,
+          archetype: "Life Domain"
+        },
+        {
+          class_id: 3,
+          archetype: "Light Domain"
+        },
+        {
+          class_id: 3,
+          archetype: "Nature Domain"
+        },
+        {
+          class_id: 3,
+          archetype: "Tempest Domain"
+        },
+        {
+          class_id: 3,
+          archetype: "Trickery Domain"
+        },
+        {
+          class_id: 3,
+          archetype: "War Domain"
+        },
+        {
+          class_id: 3,
+          archetype: "Forge Domain"
+        },
+        {
+          class_id: 3,
+          archetype: "Grave Domain"
+        }
       ]);
     });
 };
