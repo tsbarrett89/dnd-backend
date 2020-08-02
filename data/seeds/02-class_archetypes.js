df12fdfc4b1e80fbe08f3b571f3aac90a0deacca
tsbarrett89 <tsbarrett89@gmail.com>
2020-08-02 14:50:42 -0600

    class archetypes seeds finished

--- data/seeds/02-class_archetypes.js
+++ data/seeds/02-class_archetypes.js
@@ -80,6 +80,214 @@ exports.seed = function(knex) {
         {
           class_id: 3,
           archetype: "Grave Domain"
+        },
+        {
+          class_id: 4,
+          archetype: "Circle of the Land"
+        },
+        {
+          class_id: 4,
+          archetype: "Circle of the Moon"
+        },
+        {
+          class_id: 4,
+          archetype: "Circle of Dreams"
+        },
+        {
+          class_id: 4,
+          archetype: "Circle of the Sheperd"
+        },
+        {
+          class_id: 5,
+          archetype: "Champion"
+        },
+        {
+          class_id: 5,
+          archetype: "Battle Master"
+        },
+        {
+          class_id: 5,
+          archetype: "Eldritch Knight"
+        },
+        {
+          class_id: 5,
+          archetype: "Arcane Archer"
+        },
+        {
+          class_id: 5,
+          archetype: "Cavalier"
+        },
+        {
+          class_id: 5,
+          archetype: "Samurai"
+        },
+        {
+          class_id: 6,
+          archetype: "Way of the Open Hand"
+        },
+        {
+          class_id: 6,
+          archetype: "Way of Shadow"
+        },
+        {
+          class_id: 6,
+          archetype: "Way of the Four Elements"
+        },
+        {
+          class_id: 6,
+          archetype: "Way of the Drunken Master"
+        },
+        {
+          class_id: 6,
+          archetype: "Way of the Kensei"
+        },
+        {
+          class_id: 6,
+          archetype: "Way of the Sun Soul"
+        },
+        {
+          class_id: 7,
+          archetype: "Oath of Devotion"
+        },
+        {
+          class_id: 7,
+          archetype: "Oath of the Ancients"
+        },
+        {
+          class_id: 7,
+          archetype: "Oath of Vengeance"
+        },
+        {
+          class_id: 7,
+          archetype: "Oath of Conquest"
+        },
+        {
+          class_id: 7,
+          archetype: "Oath of Redemption"
+        },
+        {
+          class_id: 8,
+          archetype: "Hunter"
+        },
+        {
+          class_id: 8,
+          archetype: "Beast Master"
+        },
+        {
+          class_id: 8,
+          archetype: "Gloom Stalker"
+        },
+        {
+          class_id: 8,
+          archetype: "Horizon Walker"
+        },
+        {
+          class_id: 8,
+          archetype: "Monster Slayer"
+        },
+        {
+          class_id: 9,
+          archetype: "Thief"
+        },
+        {
+          class_id: 9,
+          archetype: "Assassin"
+        },
+        {
+          class_id: 9,
+          archetype: "Arcane Trickster"
+        },
+        {
+          class_id: 9,
+          archetype: "Inquisitive"
+        },
+        {
+          class_id: 9,
+          archetype: "Mastermind"
+        },
+        {
+          class_id: 9,
+          archetype: "Scout"
+        },
+        {
+          class_id: 9,
+          archetype: "Swashbuckler"
+        },
+        {
+          class_id: 10,
+          archetype: "Draconic Bloodline"
+        },
+        {
+          class_id: 10,
+          archetype: "Wild Magic"
+        },
+        {
+          class_id: 10,
+          archetype: "Divine Soul"
+        },
+        {
+          class_id: 10,
+          archetype: "Shadow Magic"
+        },
+        {
+          class_id: 10,
+          archetype: "Storm Sorcery"
+        },
+        {
+          class_id: 11,
+          archetype: "The Archfey"
+        },
+        {
+          class_id: 11,
+          archetype: "The Fiend"
+        },
+        {
+          class_id: 11,
+          archetype: "The Great Old One"
+        },
+        {
+          class_id: 11,
+          archetype: "The Celestial"
+        },
+        {
+          class_id: 11,
+          archetype: "The Hexblade"
+        },
+        {
+          class_id: 12,
+          archetype: "School of Abjuration"
+        },
+        {
+          class_id: 12,
+          archetype: "School of Conjuration"
+        },
+        {
+          class_id: 12,
+          archetype: "School of Divination"
+        },
+        {
+          class_id: 12,
+          archetype: "School of Enchantment"
+        },
+        {
+          class_id: 12,
+          archetype: "School of Evocation"
+        },
+        {
+          class_id: 12,
+          archetype: "School of Illusion"
+        },
+        {
+          class_id: 12,
+          archetype: "School of Necromancy"
+        },
+        {
+          class_id: 12,
+          archetype: "School of Transmutation"
+        },
+        {
+          class_id: 12,
+          archetype: "War Magic"
         }
       ]);
     });
